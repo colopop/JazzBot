e066f33afa8c1fbe72dc7aca52394846dbf8442b
colopop <colopop@users.noreply.github.com>
2017-10-08 13:09:16 -0400

    Update listener.py

--- src/listener.py
+++ src/listener.py
@@ -3,20 +3,20 @@ from fbchat.models import *
 
 playlist = []
 
-
-def add_to_playlist(link):
-	playlist.append(link)
-	print playlist
+#for testing -- this will end up adding the link to a youtube playlist.
+#def add_to_playlist(link):
+#	playlist.append(link)
+#	print playlist
 
 #subclass Client to have the behavior we want when receiving a message
 class YouTubeLinkGrabber(Client):
 	def onMessage(self,mid=None, author_id=None, message=None, thread_id=None, thread_type=ThreadType.USER, ts=None, metadata=None, msg={}):
 		#checks attachments for youtube links
 		#if thread_id == group.uid: #not implemented for testing
-		if len(msg['delta']['attachments']) > 0:
+		if len(msg['delta']['attachments']) > 0: #time to navigate the absurd structure of a facebook message. this checks for attachments.
 			for item in msg['delta']['attachments']:
 				if "youtu" in item['mercury']['share']['media']['source']: #catches youtube and youtu.be links
-					add_to_playlist(item['mercury']['share']['media']['source'])
+					add_to_playlist(item['mercury']['share']['media']['source']) 
 
 
 with open("../info/fblogin","r") as userinfo:
@@ -26,6 +26,6 @@ with open("../info/fblogin","r") as userinfo:
 client = YouTubeLinkGrabber(email, password)
 
 with open("../info/group","r") as gname:
-  group = client.searchForGroups(gname)[0]
+  group = client.searchForGroups(gname.read())[0]
 
 client.listen()
